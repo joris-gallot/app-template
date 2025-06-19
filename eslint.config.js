2dacfa1aa6662694f1c82508d0e098b9fb3319cb
Joris <joris.gallot18@gmail.com>
2025-06-19 15:58:25 +0200

    chore(workspace): add pnpm catalogs

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   vue: true,
   typescript: true,
+  yaml: true,
   rules: {
     'no-restricted-imports': ['error', {
       paths: [
