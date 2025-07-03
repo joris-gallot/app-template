177a965538f56c3b8befbb7c8be09f5fe9c517e3
Joris <joris.gallot18@gmail.com>
2025-07-03 11:00:45 +0200

    feat: set validation on blur

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,9 +1,18 @@
+import { configure } from 'vee-validate'
+
 import { createApp } from 'vue'
 
 import App from './App.vue'
-
 import router from './router'
+
 import './assets/main.css'
+
+configure({
+  validateOnBlur: true,
+  validateOnChange: false,
+  validateOnModelUpdate: false,
+  validateOnInput: false,
+})
 
 const app = createApp(App)
 
